a955cb639be99263be3fae264d76c80e4689db37
mmerayo30 <mmerayo30@gmail.com>
2013-01-07 20:10:30 +0000

    ISSUE-157: fixed main build

--- tasks_branches/ISSUE-157_create_acceptance_tests_own_folder/Code/Main/runTestsx86.cmd
+++ tasks_branches/ISSUE-157_create_acceptance_tests_own_folder/Code/Main/runTestsx86.cmd
@@ -1,2 +1,2 @@
 cd /d %~dp0
-"..\Build\Packages\nUnit\NUnit-2.5.6.10205\nunitbin\nunit-x86.exe" ermeX-x86.nunit
+"..\..\Build\Packages\nUnit\NUnit-2.5.6.10205\nunitbin\nunit-x86.exe" ermeX-x86.nunit
