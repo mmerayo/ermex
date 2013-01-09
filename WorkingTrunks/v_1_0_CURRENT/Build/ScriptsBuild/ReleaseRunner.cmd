de16bd2be4e679e3bfe2d7aa1cb097766a6ef69a
mmerayo30 <mmerayo30@gmail.com>
2013-01-09 10:54:30 +0000

    Issue-13 Added minor and revision numbers to be jenkins build num and svn revision

--- WorkingTrunks/v_1_0_CURRENT/Build/ScriptsBuild/ReleaseRunner.cmd
+++ WorkingTrunks/v_1_0_CURRENT/Build/ScriptsBuild/ReleaseRunner.cmd
@@ -19,15 +19,21 @@ set nunitExePath=%packagesFolder%\nUnit\NUnit-2.5.6.10205\nunitbin
 set taskId=%1
 set msBuildProperties=Configuration=%configuration%;OutputPath="%outputBinFolder%"\;Platform=%buildType%;Optimize=true;BuildReportsFolder="%outputReportsFolder%";TaskIdName=%taskId%;nUnitPath="%nunitExePath%";nUnitFramework=%nunitFramework%;MergedFolder="%mergedFolder%"
 
-
-
 IF NOT "%2"=="" GOTO cont
-	set msBuildProperties=%msBuildProperties%;BRevisionNumber=0
+	set msBuildProperties=%msBuildProperties%;BMinorNumber=0
 :cont
 IF "%2"=="" GOTO cont1
-	set msBuildProperties=%msBuildProperties%;BRevisionNumber=%2
+	set msBuildProperties=%msBuildProperties%;BMinorNumber=%2
 
 :cont1
+
+IF NOT "%3"=="" GOTO cont2
+	set msBuildProperties=%msBuildProperties%;BRevisionNumber=0
+:cont2
+IF "%3"=="" GOTO cont3
+	set msBuildProperties=%msBuildProperties%;BRevisionNumber=%3
+
+:cont3
 
 REM Invoking MSBuild
 ::%frameworkFolder%\msbuild /verbosity:diagnostic /fileLogger "%CD%"\RunBuild.xml /property:%msBuildProperties%
