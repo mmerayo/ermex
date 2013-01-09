199fb2b1f79af06e50acef2ce55c0e18d6d390d9
mmerayo30 <mmerayo30@gmail.com>
2013-01-09 10:42:43 +0000

    Merged revision(s) 37-51 from WorkingTrunks/v_1_0_CURRENT:

--- tasks_branches/ISSUE-227_fix_parallel_build_errors/Build/ScriptsBuild/ReleaseRunner.cmd
+++ tasks_branches/ISSUE-227_fix_parallel_build_errors/Build/ScriptsBuild/ReleaseRunner.cmd
@@ -9,14 +9,13 @@ set nunitFramework=net-%frameworkVersion%
 set frameworkFolder=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 set configuration=%CI_Configuration%
 
-
 REM COMMON TO ALL BUILDS
 set artifactsFolder=%CD%\..\BuildArtifacts
 set outputBinFolder=%artifactsfolder%\Binaries\%frameworkVersion%\%buildType%\%configuration%
 set mergedFolder=%artifactsfolder%\Merged\v%frameworkVersion%\%buildType%
 set outputReportsFolder=%artifactsfolder%\Reports\%frameworkVersion%\%buildType%
 set packagesFolder=%CD%\..\Packages
-set nunitExePath=%packagesFolder%\nUnit\NUnit-2.5.6.10205\bin
+set nunitExePath=%packagesFolder%\nUnit\NUnit-2.5.6.10205\nunitbin
 set taskId=%1
 set msBuildProperties=Configuration=%configuration%;OutputPath="%outputBinFolder%"\;Platform=%buildType%;Optimize=true;BuildReportsFolder="%outputReportsFolder%";TaskIdName=%taskId%;nUnitPath="%nunitExePath%";nUnitFramework=%nunitFramework%;MergedFolder="%mergedFolder%"
 
