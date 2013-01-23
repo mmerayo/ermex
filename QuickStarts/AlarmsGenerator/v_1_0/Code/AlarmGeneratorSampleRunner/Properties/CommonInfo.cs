2c27d59cf55b08cb418e638970c527bf6392479d
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-01-23 13:38:16 +0000

    ISSUE-29 restore point Commit

---
+++ QuickStarts/AlarmsGenerator/v_1_0/Code/AlarmGeneratorSampleRunner/Properties/CommonInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Miguel Merayo")]
+[assembly: AssemblyProduct("ermeX QuickStart Alarms Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
