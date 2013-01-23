f08f590b74bd3999d3d103d44266073419da8135
mmerayo30 <mmerayo30@gmail.com>
2013-01-23 17:20:09 +0000

    ISSUE-29 restore point Commit

---
+++ QuickStarts/AlarmsGenerator/v_1_0/Code/AlarmGeneratorSampleRunner/FrmRunner.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlarmGeneratorSampleRunner
+{
+    public partial class FrmRunner : Form
+    {
+
+        /// <summary>
+        /// Enumerates the status of the view
+        /// </summary>
+        private enum Status
+        {
+            /// <summary>
+            /// The emulation is running
+            /// </summary>
+            Running = 1,
+
+            /// <summary>
+            /// the emulation is stopped
+            /// </summary>
+            Stopped = 0
+        }
+
+        private Status CurrentStatus { get; set; }
+
+        public FrmRunner()
+        {
+            InitializeComponent();
+
+            SetStatus(Status.Stopped);
+        }
+
+        private void SetStatus(Status newStatus)
+        {
+            if (CurrentStatus == newStatus)
+                return;
+
+            CurrentStatus = newStatus;
+
+            btnStart.Enabled = CurrentStatus == Status.Stopped;
+            btnNewMachine.Enabled = CurrentStatus == Status.Running;
+            btnNewPanel.Enabled = CurrentStatus == Status.Running;
+            btnStop.Enabled = CurrentStatus == Status.Running;
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //performs the default start, one vendor machine and one stock alarm panel
+
+                //changes the status
+                SetStatus(Status.Running);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex.ToString());
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Finish the processes
+
+                //changes the status
+                SetStatus(Status.Stopped);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex.ToString());
+            }
+        }
+
+        private void btnNewMachine_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Adds a new machine by creating a process that emulates the device
+
+                //changes the status
+                SetStatus(Status.Stopped);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex.ToString());
+            }
+        }
+
+        private void btnNewPanel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Adds a new machine by creating a process that emulates the device
+
+                //changes the status
+                SetStatus(Status.Stopped);
+            }
+            catch (Exception ex)
+            {
+                OnError(ex.ToString());
+            }
+        }
+
+        private void OnError(string message)
+        {
+            MessageBox.Show(message, "An error happened:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
--- QuickStarts/AlarmsGenerator/v_1_0/Code/AlarmGeneratorSampleRunner/Program.cs
+++ QuickStarts/AlarmsGenerator/v_1_0/Code/AlarmGeneratorSampleRunner/Program.cs
@@ -15,7 +15,7 @@ namespace AlarmGeneratorSampleRunner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmRunner());
         }
     }
 }
