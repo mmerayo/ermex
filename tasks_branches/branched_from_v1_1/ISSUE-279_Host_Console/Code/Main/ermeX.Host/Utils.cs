a12361e6e5a06038f62415a47f52958cfb37384d
dishansm <dishansm@gmail.com>
2013-02-16 10:14:53 +0000

    Basic Structure of the Host

---
+++ tasks_branches/branched_from_v1_1/ISSUE-279_Host_Console/Code/Main/ermeX.Host/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.Host
+{
+    /// <summary>
+    /// Utility class.
+    /// </summary>
+    internal static class Utils
+    {
+        internal static Command GetCommand(string arg)
+        {
+            string[] components = arg.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (components.Length < 1)
+            {
+                return null;
+            }
+            else
+            {
+                Command command = new Command();
+                command.CommandString = components[0];
+                command.ParameterString = components[1];
+                return command;
+            }
+
+        }
+    }
+}
