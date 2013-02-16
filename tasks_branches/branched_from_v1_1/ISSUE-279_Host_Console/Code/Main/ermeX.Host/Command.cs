e9e7f468e26362bac6241724163cdfa3ddb9d280
dishansm@gmail.com <dishansm@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-02-16 10:14:53 +0000

    Basic Structure of the Host

---
+++ tasks_branches/branched_from_v1_1/ISSUE-279_Host_Console/Code/Main/ermeX.Host/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.Host
+{
+    internal class Command
+    {
+        internal string CommandString { get; set; }
+        internal string ParameterString { get; set; }
+    }
+}
