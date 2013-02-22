1e284ff11e721b19ff46c0db2b4d6702a2e69ea2
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-02-22 08:42:11 +0000

    ISSUE-277 ADDED SOME LIBS

---
+++ tasks_branches/branched_from_v1_1/ISSUE-277_Improve_Pack_and_nugetTask/Build/Packages/PublicPackageGenerator/PublicPackageGenerator/PublicPackageGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PublicPackageGenerator
+{
+    [TestFixture]
+    public class PublicPackageGeneratorTests
+    {
+        [Test]
+        public void CanSaveCurrentBuild()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GeneratesPackage_WhenCurrentBuildIsTheLastOfTheExpected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
