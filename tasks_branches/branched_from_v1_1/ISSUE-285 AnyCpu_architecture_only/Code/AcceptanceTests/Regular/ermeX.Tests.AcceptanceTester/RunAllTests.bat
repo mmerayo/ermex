d2ab4bc1517c20947a87abd66739363c81046e0f
mmerayo30 <mmerayo30@gmail.com>
2013-02-27 16:22:09 +0000

    ISSUE-285 renamed to avoid spaces in path

--- tasks_branches/branched_from_v1_1/ISSUE-285 AnyCpu_architecture_only/Code/AcceptanceTests/Regular/ermeX.Tests.AcceptanceTester/RunAllTests.bat
+++
@@ -1,1 +0,0 @@
-﻿ermeX.Tests.AcceptanceTester.exe AllJoinToSameFriendComponent true 60000 61000 5 150 false 0
--- tasks_branches/branched_from_v1_1/ISSUE-285 AnyCpu_architecture_only/Code/Main/ermeX.DAL.Interfaces/Observer/IDalObservable.cs
+++
@@ -1,9 +0,0 @@
-namespace ermeX.DAL.Interfaces.Observer
-{
-    internal interface IDalObservable<TEntity>
-    {
-        void AddObserver(IDalObserver<TEntity> observer);
-        void RemoveObserver(IDalObserver<TEntity> observer);
-        void Notify(NotifiableDalAction action, TEntity entity);
-    }
-}
