9e177647eb4c1b96a304ffe01138d081f23e47b6
d.conghoang@gmail.com <d.conghoang@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-03-03 11:10:23 +0000

    ISSUE-291_ermeX.Gateway_Step1: Task done: - Folder structure - Configuration Section - Models for RESTful web service code generation

---
+++ tasks_branches/branched_from_v1_1/ISSUE-291_ermeX.Gateway_Step1/Code/Components/ermeX.Gateway/ermeX.Gateway.CodeGen.Restful.Models/Document.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.Gateway.CodeGen.Restful.Models
+{
+    public class Document
+    {
+        public Dictionary<string, string> Namespace { get; set; }
+        public List<Schema> Schema { get; set; }
+        public Interface Interface { get; set; }
+        public Binding Binding { get; set; }
+        public Service Service { get; set; }
+    }
+}
---
+++ tasks_branches/branched_from_v1_1/ISSUE-291_ermeX.Gateway_Step1/Code/Components/ermeX.Gateway/ermeX.Gateway.CodeGen.Restful.Models/Schema.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.Gateway.CodeGen.Restful.Models
+{
+    public class Schema
+    {
+        public string Name { get; set; }
+
+        public List<SchemaElement> Elements { get; set; }
+    }
+}
