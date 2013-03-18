033f73ca78ca7bf7e4affa3d41d424296d335ba3
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-03-18 09:42:41 +0000

    ISSUE-281: branch commit.Restore point

---
+++ tasks_branches/branched_from_v1_1/ISSUE-281_UnitOfWork/Code/Main/ermeX.Domain/AppComponent/ICanReadComponents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ermeX.Domain.AppComponent
+{
+    internal interface ICanReadComponents
+    {
+        Entities.Entities.AppComponent Fetch(Guid componentId);
+        List<Entities.Entities.AppComponent> FetchOtherComponents();
+        List<Entities.Entities.AppComponent> FetchOtherComponentsNotExchangedDefinitions(bool running = false);
+
+    }
+}
