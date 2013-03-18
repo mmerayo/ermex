9267044cdad9e7526ed2d76ba3537f5be4d11fe7
mmerayo30 <mmerayo30@gmail.com>
2013-03-18 09:42:41 +0000

    ISSUE-281: branch commit.Restore point

---
+++ tasks_branches/branched_from_v1_1/ISSUE-281_UnitOfWork/Code/Main/ermeX.Domain/AppComponent/Impl/ComponentsReader.cs
@@ -0,0 +1,14 @@
+using System;
+using ermeX.Domain.AppComponent;
+using ermeX.Entities.Entities;
+
+namespace ermeX.Domain.Implementations
+{
+    internal class ComponentsReader : ICanReadComponents
+    {
+        public AppComponent Fetch(Guid componentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
