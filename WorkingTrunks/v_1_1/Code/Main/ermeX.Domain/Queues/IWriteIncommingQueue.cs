8dfc3dbf9f80a5eca2f2b0c7e892220cd0b9aa50
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-04-01 16:54:32 +0000

    [https://www.pivotaltracker.com/story/show/47176175]: Merged from branch commit. Simplified Dal commands interface

--- WorkingTrunks/v_1_1/Code/Main/ermeX.Domain/Queues/IWriteIncommingQueue.cs
+++ WorkingTrunks/v_1_1/Code/Main/ermeX.Domain/Queues/IWriteIncommingQueue.cs
@@ -3,11 +3,11 @@ using ermeX.Entities.Entities;
 
 namespace ermeX.Domain.Queues
 {
-	interface IWriteIncommingQueue
-    {
-        //TODO: Create queue interface and call this method as Push
-		void Save(IncomingMessage incomingMessage);
+	internal interface IWriteIncommingQueue
+	{
+		//TODO: Create queue interface and call this method as Push
 		void Remove(IncomingMessage incomingMessage);
+		void Save(IncomingMessage incomingMessage);
 		void Save(IEnumerable<IncomingMessage> incomingMessages);
-    }
+	}
 }
