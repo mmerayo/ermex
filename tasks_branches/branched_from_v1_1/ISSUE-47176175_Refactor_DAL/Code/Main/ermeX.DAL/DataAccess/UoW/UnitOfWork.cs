97d0a0c7e6d4bbcfe1dd532b895269cb90c8d6c5
mmerayo30 <mmerayo30@gmail.com>
2013-04-02 23:16:45 +0000

    [https://www.pivotaltracker.com/story/show/47176175]. Branch commit. Added unit of work without tests

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/DataAccess/UoW/UnitOfWork.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/DataAccess/UoW/UnitOfWork.cs
@@ -6,30 +6,39 @@ namespace ermeX.DAL.DataAccess.UoW
 	internal static class UnitOfWork
 	{
 		private static IUnitOfWorkFactory _unitOfWorkFactory;
-		private static IUnitOfWork _innerUnitOfWork;
 
 		public static IUnitOfWork Start()
 		{
-			if (_innerUnitOfWork != null)
+			if (CurrentUnitOfWork != null)
 				throw new InvalidOperationException("You cannot start more than one unit of work at the same time.");
 
-			_innerUnitOfWork = _unitOfWorkFactory.Create();
-			return _innerUnitOfWork;
+			var unitOfWork = _unitOfWorkFactory.Create();
+			CurrentUnitOfWork = unitOfWork;
+			return unitOfWork;
+		}
+
+		public const string CurrentUnitOfWorkKey = "ermeX.UnitOfWork";
+
+		private static IUnitOfWork CurrentUnitOfWork
+		{
+			get { return Local.Data[CurrentUnitOfWorkKey] as IUnitOfWork; }
+			set { Local.Data[CurrentUnitOfWorkKey] = value; }
 		}
 
 		public static IUnitOfWork Current
 		{
 			get
 			{
-				if (_innerUnitOfWork == null)
-					throw new InvalidOperationException("You are not in a unit of work.");
-				return _innerUnitOfWork;
+				var unitOfWork = CurrentUnitOfWork;
+				if (unitOfWork == null)
+					throw new InvalidOperationException("You are not in a unit of work");
+				return unitOfWork;
 			}
 		}
 
 		public static bool IsStarted
 		{
-			get { return _innerUnitOfWork != null; }
+			get { return CurrentUnitOfWork != null; }
 		}
 
 		public static ISession CurrentSession
@@ -40,7 +49,7 @@ namespace ermeX.DAL.DataAccess.UoW
 
 		public static void DisposeUnitOfWork(UnitOfWorkImplementor adapter)
 		{
-			_innerUnitOfWork = null;
+			CurrentUnitOfWork = null;
 		}
 	}
 }
