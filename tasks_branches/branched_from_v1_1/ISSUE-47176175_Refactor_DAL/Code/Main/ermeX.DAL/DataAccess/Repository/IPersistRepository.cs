9c708fc04ec60513bec03b53671b6061b05275f4
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-04-03 12:39:43 +0000

    ISSUE-47176175: Branch commit.Before repository

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/DataAccess/Repository/IPersistRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using ermeX.Entities.Base;
+
+namespace ermeX.DAL.DataAccess.Repository
+{
+	internal interface IPersistRepository<TEntity> where TEntity : ModelBase
+	{
+		bool Save(TEntity entity);
+		bool Save(IEnumerable<TEntity> items);
+		bool Remove(int id);
+		bool Remove(TEntity entity);
+		bool Remove(IEnumerable<TEntity> entities);
+		bool Remove(Expression<Func<TEntity, bool>> expression);
+	}
+}
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/DataAccess/Repository/Repository.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NHibernate;
+using NHibernate.Linq;
+using Remotion.Linq.Utilities;
+using ermeX.DAL.DataAccess.UoW;
+using ermeX.Entities.Base;
+
+namespace ermeX.DAL.DataAccess.Repository
+{
+	internal abstract class Repository<TEntity> : IPersistRepository<TEntity>,
+		IReadOnlyRepository<TEntity> where TEntity : ModelBase
+	{
+		private readonly Guid _localComponentId;
+		private readonly IUnitOfWorkFactory _factory;
+
+		//Expresion to find an entity other than the localkey
+		protected abstract Expression<Func<TEntity, bool>> FindByBizKey { get; } //TODO: REMOVE see beforeupdating
+
+		protected Repository(Guid localComponentId, 
+			IUnitOfWorkFactory factory)
+		{
+			if (localComponentId==Guid.Empty)
+				throw new ArgumentEmptyException("localComponentId");
+			if (factory == null) throw new ArgumentNullException("factory");
+			_localComponentId = localComponentId;
+			_factory = factory;
+		}
+
+		public bool Save(TEntity entity)
+		{
+			if (entity.ComponentOwner == Guid.Empty)
+				throw new ArgumentEmptyException("entity.ComponentOwner");
+
+			if (!CanSave(entity))
+				return false;
+
+			if (entity.ComponentOwner == _localComponentId)
+				entity.Version = DateTime.UtcNow.Ticks; //Keeps the version of the last updater
+
+			entity.ComponentOwner = _localComponentId;
+
+			_factory.CurrentSession.SaveOrUpdate(entity);
+			return true;
+		}
+
+		private bool CanSave(TEntity entity)
+		{
+			var item = SingleOrDefault(FindByBizKey);
+			return item == null || item.Version <= entity.Version; //Can save if is null or the version is newer
+		}
+
+		public bool Save(IEnumerable<TEntity> items)
+		{
+			foreach (TEntity item in items)
+			{
+				Save(item);
+			}
+			return true;
+		}
+
+		public bool Remove(int id)
+		{
+			var toRemove = Single(id);
+			Remove(toRemove);
+			return true;
+		}
+
+		public bool Remove(TEntity entity)
+		{
+			_factory.CurrentSession.Delete(entity);
+			return true;
+		}
+
+		public bool Remove(IEnumerable<TEntity> entities)
+		{
+			foreach (var entity in entities)
+				Remove(entity);
+			return true;
+		}
+
+		public bool Remove(Expression<Func<TEntity, bool>> expression)
+		{
+			IQueryable<TEntity> queryable = Where(expression);
+			Remove(queryable);
+			return true;
+		}
+
+		public IQueryable<TEntity> FetchAll()
+		{
+			return _factory.CurrentSession.Query<TEntity>();
+		}
+
+		public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> expression)
+		{
+			return Where(expression).SingleOrDefault();
+		}
+
+		public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> expression)
+		{
+			return _factory.CurrentSession.Query<TEntity>().Where(expression);
+		}
+
+		public TEntity Single(int id)
+		{
+			return _factory.CurrentSession.Get<TEntity>(id);
+		}
+	}
+}
