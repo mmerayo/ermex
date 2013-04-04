7f227bdbbeaef0250f24f489450127636bca5935
mmerayo30 <mmerayo30@gmail.com>
2013-04-04 16:03:05 +0000

    ISSUE-47176175: Branch commit.Before removing old datasource

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Domain.Implementations/Subscriptions/CanUpdateOutgoingMessagesSubscriptions.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Domain.Implementations/Subscriptions/CanUpdateOutgoingMessagesSubscriptions.cs
@@ -4,26 +4,33 @@ using Ninject;
 using ermeX.ConfigurationManagement.Settings;
 using ermeX.DAL.DataAccess.UoW;
 using ermeX.DAL.Interfaces;
+using ermeX.DAL.Interfaces.Observer;
+using ermeX.Domain.Implementations.Observers;
+using ermeX.Domain.Observers;
 using ermeX.Domain.Subscriptions;
 using ermeX.Entities.Entities;
 
 namespace ermeX.Domain.Implementations.Subscriptions
 {
+
+	
 	class CanUpdateOutgoingMessagesSubscriptions : ICanUpdateOutgoingMessagesSubscriptions
 	{
 		private readonly IPersistRepository<OutgoingMessageSuscription> _repository;
 		private readonly IUnitOfWorkFactory _factory;
 		private readonly IComponentSettings _settings;
+		private readonly DomainNotifier _domainNotifier; //TODO: REMOVE THIS MECHANISM
 
 		[Inject]
 		public CanUpdateOutgoingMessagesSubscriptions(
 			IPersistRepository<OutgoingMessageSuscription> repository,
 			IUnitOfWorkFactory factory,
-			IComponentSettings settings)
+			IComponentSettings settings, IDomainObservable domainNotifier )
 		{
 			_repository = repository;
 			_factory = factory;
 			_settings = settings;
+			_domainNotifier =(DomainNotifier) domainNotifier;
 		}
 
 
@@ -46,15 +53,19 @@ namespace ermeX.Domain.Implementations.Subscriptions
 
 		public void ImportFromOtherComponent(OutgoingMessageSuscription susbcription)
 		{
+			bool isNew = false;
+			var subscriptionToSave = susbcription;
+
 			using (var uow = _factory.Create())
 			{
-				var subscriptionToSave = susbcription;
 
 				Expression<Func<OutgoingMessageSuscription, bool>> expression =
 					x => x.Component == susbcription.ComponentOwner && x.BizMessageFullTypeName == susbcription.BizMessageFullTypeName;
+
 				if (!_repository.Any(expression))
 				{
 					subscriptionToSave.Id = 0;
+					isNew = true;
 				}
 				else
 				{
@@ -64,7 +75,11 @@ namespace ermeX.Domain.Implementations.Subscriptions
 				}
 				_repository.Save(subscriptionToSave);
 				uow.Commit();
+
 			}
+
+			//TODO: THIS AND ITS MECHANISM MUST BE REMOVED
+			_domainNotifier.Notify(isNew ? NotifiableDalAction.Add : NotifiableDalAction.Update, subscriptionToSave);
 		}
 	}
 }
