e057ffe565ccb4fb376c6052b929e1db8cccaed0
mmerayo30 <mmerayo30@gmail.com>
2013-04-13 10:32:08 +0000

    [https://www.pivotaltracker.com/story/show/47176175]. Branch commit. Added unit of work without tests

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Tests.DAL.Integration/DataSources/OutgoingMessageSuscriptionsSourceTester.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Tests.DAL.Integration/DataSources/OutgoingMessageSuscriptionsSourceTester.cs
@@ -82,7 +82,7 @@ namespace ermeX.Tests.DAL.Integration.DataSources
 			{
 				var repository = GetRepository<Repository<AppComponent>>();
 				var repo = repository;
-				appComponent = repo.Single(uow.Session, idComponent);
+				appComponent = repo.Single(uow, idComponent);
 				uow.Commit();
 			}
 
