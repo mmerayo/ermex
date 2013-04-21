6f444a63eb2906599e52b13a60a2ac7eb9ed25b2
mmerayo30 <mmerayo30@gmail.com>
2013-04-21 19:32:58 +0000

    [https://www.pivotaltracker.com/story/show/47176175]. Branch commit. Added retryable operations command decorator pending

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/UnitOfWork/IUnitOfWorkFactory.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/UnitOfWork/IUnitOfWorkFactory.cs
@@ -1,7 +1,12 @@
-﻿namespace ermeX.DAL.UnitOfWork
+﻿using System;
+
+namespace ermeX.DAL.UnitOfWork
 {
 	internal interface IUnitOfWorkFactory
 	{
 		IUnitOfWork Create(bool autoCommitWhenDispose=false);
+
+		TResult ExecuteInUnitOfWork<TResult>(Func<TResult> atomicFunction);
+		void ExecuteInUnitOfWork(Action atomicAction);
 	}
 }
