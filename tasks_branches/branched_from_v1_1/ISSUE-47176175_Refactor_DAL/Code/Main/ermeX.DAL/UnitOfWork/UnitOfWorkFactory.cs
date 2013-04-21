7fa7d72c965510ffd2ed84d837b69cd8e49f1588
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-04-21 19:32:58 +0000

    [https://www.pivotaltracker.com/story/show/47176175]. Branch commit. Added retryable operations command decorator pending

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/UnitOfWork/UnitOfWorkFactory.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.DAL/UnitOfWork/UnitOfWorkFactory.cs
@@ -1,6 +1,14 @@
-﻿using System.Threading;
+﻿using System;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Threading;
 using Common.Logging;
+using NHibernate;
+using NHibernate.Exceptions;
 using Ninject;
+using ermeX.ConfigurationManagement.Settings;
+using ermeX.ConfigurationManagement.Settings.Data.DbEngines;
 using ermeX.DAL.Providers;
 
 namespace ermeX.DAL.UnitOfWork
@@ -9,20 +17,164 @@ namespace ermeX.DAL.UnitOfWork
 	{
 		private static readonly ILog Logger = LogManager.GetLogger(typeof(UnitOfWorkFactory).FullName);
 		private readonly ISessionProvider _sessionFactory;
+		private readonly IStorageOperationRetryStrategy _retryStrategy;
 
 		[Inject]
-		public UnitOfWorkFactory(ISessionProvider sessionProvider)
+		public UnitOfWorkFactory(ISessionProvider sessionProvider,IDalSettings settings)
 		{
 			Logger.DebugFormat("cctor: thread={0}",Thread.CurrentThread.ManagedThreadId);
 			_sessionFactory = sessionProvider;
+
+			const int retriesDeadlock = 15;//TODO: TO DAL SETTINGS
+			const int retriesTimeout = 3;
+			switch(settings.ConfigurationSourceType)
+			{
+				case DbEngineType.SqlServer2008:
+					_retryStrategy = new SqlServerRetryPolicy(retriesDeadlock, retriesTimeout);
+					break;
+				case DbEngineType.Sqlite:
+				case DbEngineType.SqliteInMemory:
+					_retryStrategy = new SqliteRetryPolicy(retriesDeadlock, retriesTimeout);
+					break;
+				default:
+					throw new NotSupportedException(settings.ConfigurationSourceType.ToString());
+			}
 		}
 
 		public IUnitOfWork Create (bool autoCommitWhenDispose=false)
 		{
 			Logger.DebugFormat("Create. autoCommitWhenDispose={0} Thread={1}",autoCommitWhenDispose,Thread.CurrentThread.ManagedThreadId);
 			var session = _sessionFactory.OpenSession();
-			//session.FlushMode = FlushMode.Always;
+			session.FlushMode = FlushMode.Commit;
 			return new UnitOfWorkImplementor(this, session,autoCommitWhenDispose);
 		}
+
+		public void ExecuteInUnitOfWork(Action atomicAction)
+		{
+			ExecuteInUnitOfWork(() =>
+			                    	{
+			                    		atomicAction();
+			                    		return new object();
+			                    	});
+		}
+
+		public TResult ExecuteInUnitOfWork<TResult>(Func<TResult> atomicFunction)
+		{
+			const int millisecondsRetry = 500;
+
+			TResult result;
+			while (true)
+			{
+				using (var uow = Create())
+				{
+					try
+					{
+						result=atomicFunction();
+						uow.Commit();
+						return result;
+					}
+					catch (ADOException e)
+					{
+						try
+						{
+							if (!uow.Session.Transaction.WasRolledBack)
+								uow.Session.Transaction.Rollback();
+						}
+						catch (TransactionException ex)
+						{
+							Logger.WarnFormat("ExecuteInUnitOfWork. {0}",ex);
+						}
+						var dbException = ADOExceptionHelper.ExtractDbException(e);
+
+						if (_retryStrategy.Retry(dbException))
+						{
+							Thread.Sleep(millisecondsRetry);
+							continue;
+						}
+
+						Logger.Error("ExecuteInUnitOfWork: wont be retried");
+
+						throw;
+					}
+				}
+			}
+		}
+		private interface IStorageOperationRetryStrategy
+		{
+			bool Retry(DbException dbException);
+		}
+
+		private class SqlServerRetryPolicy : IStorageOperationRetryStrategy
+		{
+			private const int SqlDeadLockErrorNumber = 1205;
+			private const int SqlTimeOutErrorNumber = -2;
+			private int _triesDeadlock;
+			private int _triesTimeout;
+
+			public SqlServerRetryPolicy(int triesDeadlock, int triesTimeout = 1)
+			{
+				if (triesDeadlock < 1) throw new ArgumentOutOfRangeException("triesDeadlock");
+				if (triesTimeout < 1) throw new ArgumentOutOfRangeException("triesTimeout");
+				_triesDeadlock = triesDeadlock;
+				_triesTimeout = triesTimeout;
+			}
+
+			public bool Retry(DbException dbException)
+			{
+				var sqlException = dbException as SqlException;
+				if (sqlException == null)
+					throw new ArgumentException("dbException must be a not null SqlException");
+
+				return NeedRetry(sqlException);
+			}
+
+			private bool NeedRetry(SqlException ex)
+			{
+				if (ex.Number == SqlDeadLockErrorNumber)
+					return --_triesDeadlock > 0;
+				if (ex.Number == SqlTimeOutErrorNumber)
+					return --_triesTimeout > 0;
+				return false;
+			}
+		}
+
+		private class SqliteRetryPolicy : IStorageOperationRetryStrategy
+		{
+
+			private int _triesDeadlock;
+			private int _triesTimeout;
+
+			public SqliteRetryPolicy(int triesDeadlock, int triesTimeout = 1)
+			{
+				if (triesDeadlock < 1) throw new ArgumentOutOfRangeException("triesDeadlock");
+				if (triesTimeout < 1) throw new ArgumentOutOfRangeException("triesTimeout");
+				_triesDeadlock = triesDeadlock;
+				_triesTimeout = triesTimeout;
+			}
+
+			public bool Retry(DbException dbException)
+			{
+				var sqlException = dbException as SQLiteException;
+				if (sqlException == null)
+					throw new ArgumentException("dbException must be a not null SqlException");
+
+				return NeedRetry(sqlException);
+			}
+
+			private bool NeedRetry(SQLiteException ex)
+			{
+				switch ((SQLiteErrorCode)ex.ErrorCode)
+				{
+					case SQLiteErrorCode.Busy:
+						return --_triesTimeout > 0;
+					case SQLiteErrorCode.Locked:
+						return --_triesDeadlock > 0;
+					default:
+						return false;
+				}
+			}
+
+		}
 	}
+
 }
