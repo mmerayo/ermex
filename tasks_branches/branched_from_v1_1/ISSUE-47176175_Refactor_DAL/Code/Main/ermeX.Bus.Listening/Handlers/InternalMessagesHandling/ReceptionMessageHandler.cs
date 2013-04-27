e7a7d97c36b3fdceda45cabf8d72383e13ec7e06
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-04-27 19:08:59 +0000

    [https://www.pivotaltracker.com/story/show/47176175]: branch commit. Acceptance tests a little bit better

--- tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Bus.Listening/Handlers/InternalMessagesHandling/ReceptionMessageHandler.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47176175_Refactor_DAL/Code/Main/ermeX.Bus.Listening/Handlers/InternalMessagesHandling/ReceptionMessageHandler.cs
@@ -101,7 +101,7 @@ namespace ermeX.Bus.Listening.Handlers.InternalMessagesHandling
 
 		public override object Handle(TransportMessage message)
 		{
-			Logger.DebugFormat("Handle",message);
+			Logger.DebugFormat("Handle: Start MessageId: {0}",message.MessageId);
 			BusMessage busMessage = message.Data;
 
 			var incomingMessage = new IncomingMessage(BusMessage.Clone(busMessage))
@@ -112,13 +112,14 @@ namespace ermeX.Bus.Listening.Handlers.InternalMessagesHandling
 					TimeReceivedUtc = DateTime.UtcNow,
 					SuscriptionHandlerId = Guid.Empty,
 					Status = Message.MessageStatus.ReceiverReceived,
+					MessageId = message.Data.MessageId
 				};
 
 
 			//this must be done on-line in case of errors so it returns an exception to the caller
 			_queueWritter.Save(incomingMessage);
 			ReceptionMessageDistributor.EnqueueItem(new ReceptionMessageDistributor.MessageDistributorMessage(incomingMessage));
-			Logger.Trace(x => x("Handle.{0} - Message received ", message.Data.MessageId));
+			Logger.Trace(x => x("Handle.{0} - Message received ", incomingMessage.MessageId));
 			return null; //Check the correctness of this null
 		}
 
