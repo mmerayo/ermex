e4182a7f35ad8dfca77587963128164e0d3dfbf8
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-05 18:12:29 +0000

    [ISSUE-47193049_StateMachines]:Branch commit. Added project ermeX.ComponentServices

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.ComponentServices
+{
+	public class ComponentManager
+	{
+		//singleton
+		private static readonly ComponentManager Default=new ComponentManager();
+		private ComponentManager()
+		{}
+
+
+
+	}
+}
