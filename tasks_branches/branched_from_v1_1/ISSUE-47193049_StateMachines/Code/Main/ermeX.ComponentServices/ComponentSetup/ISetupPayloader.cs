5aa79f0f568e417e90833b9729b972f1a73bc9cd
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-05 20:18:38 +0000

    [ISSUE-47193049_StateMachines]:Branch commit. Added test project for ermeX.ComponentServices

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentSetup/ISetupPayloader.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentSetup/ISetupPayloader.cs
@@ -1,9 +1,65 @@
+using System;
+using Stateless;
+
 namespace ermeX.ComponentServices.ComponentSetup
 {
 	internal interface ISetupPayloader
 	{
-		void InjectServices(object o);
-		void RunUpgrades(object o);
-		void HandleError(object o);
+		void InjectServices(StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent>.Transition o);
+		void RunUpgrades(StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent>.Transition o);
+		void HandleError(Exception ex);
+		
+		void SetMachine(StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent> machine);
+		void TryFire(SetupMachine.SetupEvent e);
+	}
+
+	class SetupPayloader : ISetupPayloader
+	{
+
+
+		private SetupMachine _machine;
+		
+
+		public void InjectServices(StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent>.Transition o)
+		{
+			try
+			{
+			}
+			catch(Exception ex)
+			{
+				_machine.Fire(SetupMachine.SetupEvent.Error,ex.ToString());
+			}
+			TryFire(SetupMachine.SetupEvent.Injected);
+		}
+
+		public void RunUpgrades(StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent>.Transition o)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void HandleError(Exception ex)
+		{
+			if (ex == null) throw new ArgumentNullException("ex","Use FireError from machine");
+
+			//TODO: LOG??
+			throw ex;
+		}
+
+
+		public void SetMachine(SetupMachine machine)
+		{
+			_machine = machine;
+		}
+
+		public void TryFire(SetupMachine.SetupEvent e)
+		{
+			if (_machine==null)
+				throw new InvalidOperationException("Invoke SetMachine first");
+			if(!_machine.CanFire(e))
+				throw new InvalidOperationException(string.Format("Cannot transit from {0}",_machine.State));
+			_machine.Fire(e);
+		}
+
+		
 	}
 }
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/SetupMachineTester.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using ermeX.ComponentServices.ComponentSetup;
+
+namespace ermeX.Tests.ComponentServices
+{
+	//this is tests the setupmachine integrated with the payloader
+	[TestFixture]
+	public class SetupMachineTester
+	{
+
+		[Test]
+		public void CanSetup()
+		{
+			var context=new TestContext();
+			var target = new SetupMachine(context.Payloader);
+			target.Setup();
+
+			Assert.AreEqual(SetupMachine.SetupProcessState.Ready, target.State);
+		}
+
+		[Test]
+		public void OnErrorThrows()
+		{
+			throw new NotImplementedException("Do for all states");
+		}
+
+		private class TestContext
+		{
+			public TestContext()
+			{
+				Payloader=new SetupPayloader();
+			}
+
+			public ISetupPayloader Payloader { get; private set; }
+		}
+	}
+}
