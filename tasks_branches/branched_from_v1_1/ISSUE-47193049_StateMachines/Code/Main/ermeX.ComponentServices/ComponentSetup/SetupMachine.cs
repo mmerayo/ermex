6a40a2f39af54d46b74896684812bd1125c8c970
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-05 19:27:08 +0000

    [ISSUE-47193049_StateMachines]: Branch commit. Added ComponentServices unit tests

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentSetup/SetupMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stateless;
+
+namespace ermeX.ComponentServices.ComponentSetup
+{
+	internal sealed class SetupMachine
+	{
+		private readonly ISetupPayloader _payloader;
+
+		private enum SetupProcessState
+		{
+			NotStarted = 0,
+			InjectingServices,
+			ServicesInjected,
+			Upgrading,
+			Ready,
+			Error
+		}
+
+		private enum SetupEvent
+		{
+			Inject,
+			Injected,
+			Upgrade,
+			Upgraded,
+			Error
+		}
+
+		private readonly StateMachine<SetupProcessState, SetupEvent> _machine= new StateMachine<SetupProcessState, SetupEvent>(SetupProcessState.NotStarted);
+
+		public SetupMachine(ISetupPayloader payloader)
+		{
+			if (payloader == null) throw new ArgumentNullException("payloader");
+			_payloader = payloader;
+		}
+
+		public void Setup()
+		{
+			DefineStateMachineTransitions();
+
+			_machine.Fire(SetupEvent.Inject);
+		}
+
+		private void DefineStateMachineTransitions()
+		{
+			_machine.Configure(SetupProcessState.NotStarted)
+				.Permit(SetupEvent.Inject, SetupProcessState.InjectingServices);
+
+			_machine.Configure(SetupProcessState.InjectingServices)
+				.OnEntry(_payloader.InjectServices)
+				.Permit(SetupEvent.Injected, SetupProcessState.ServicesInjected)
+				.Permit(SetupEvent.Error, SetupProcessState.Error);
+
+			_machine.Configure(SetupProcessState.ServicesInjected)
+				.Permit(SetupEvent.Upgrade, SetupProcessState.Upgrading);
+
+			_machine.Configure(SetupProcessState.Upgrading)
+				.OnEntry(_payloader.RunUpgrades)
+				.Permit(SetupEvent.Upgraded, SetupProcessState.Ready)
+				.Permit(SetupEvent.Error, SetupProcessState.Error);
+
+			_machine.Configure(SetupProcessState.Error)
+				.OnEntry(_payloader.HandleError);
+		}
+	}
+}
