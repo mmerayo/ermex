439c2e6d32ca19233e5c2873d6e9b10b7e71af83
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-06 08:47:50 +0000

    [ISSUE-47193049_StateMachines]: Branch commit. Added few unit tests for SetUpMachine

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentSetup/SetupMachine.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/ComponentSetup/SetupMachine.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Stateless;
+using ermeX.Exceptions;
 
 namespace ermeX.ComponentServices.ComponentSetup
 {
 	internal sealed class SetupMachine
 	{
-		private readonly ISetupPayloader _payloader;
+		public enum SetupEvent
+		{
+			Inject,
+			Injected,
+			Upgrade,
+			Upgraded,
+			Error
+		}
 
 		public enum SetupProcessState
 		{
@@ -20,22 +25,18 @@ namespace ermeX.ComponentServices.ComponentSetup
 			Error
 		}
 
-		public enum SetupEvent
-		{
-			Inject,
-			Injected,
-			Upgrade,
-			Upgraded,
-			Error
-		}
+		private readonly StateMachine<SetupProcessState, SetupEvent> _machine =
+			new StateMachine<SetupProcessState, SetupEvent>(SetupProcessState.NotStarted);
 
-		private readonly StateMachine<SetupProcessState, SetupEvent> _machine= new StateMachine<SetupProcessState, SetupEvent>(SetupProcessState.NotStarted);
+		private readonly ISetupPayloader _payloader;
+
+		private StateMachine<SetupProcessState, SetupEvent>.TriggerWithParameters<Exception>
+			_errorTrigger;
 
 		public SetupMachine(ISetupPayloader payloader)
 		{
 			if (payloader == null) throw new ArgumentNullException("payloader");
 			_payloader = payloader;
-			_payloader.SetMachine(_machine);
 		}
 
 		public SetupProcessState State
@@ -53,35 +54,90 @@ namespace ermeX.ComponentServices.ComponentSetup
 		private void DefineStateMachineTransitions()
 		{
 			_machine.Configure(SetupProcessState.NotStarted)
-				.OnEntry((e)=>_payloader.TryFire(SetupEvent.Inject))
+				.OnEntry(OnNotStarted)
 				.Permit(SetupEvent.Inject, SetupProcessState.InjectingServices);
 
 			_machine.Configure(SetupProcessState.InjectingServices)
-				.OnEntry(_payloader.InjectServices)
+				.OnEntry(OnInjectServices)
 				.Permit(SetupEvent.Injected, SetupProcessState.ServicesInjected)
 				.Permit(SetupEvent.Error, SetupProcessState.Error);
 
 			_machine.Configure(SetupProcessState.ServicesInjected)
-				.OnEntry((a)=> _payloader.TryFire(SetupEvent.Upgrade))
+				.OnEntry(OnServicesInjected)
 				.Permit(SetupEvent.Upgrade, SetupProcessState.Upgrading);
 
 			_machine.Configure(SetupProcessState.Upgrading)
-				.OnEntry(_payloader.RunUpgrades)
+				.OnEntry(OnUpgrading)
 				.Permit(SetupEvent.Upgraded, SetupProcessState.Ready)
 				.Permit(SetupEvent.Error, SetupProcessState.Error);
 
-			_machine.Configure(SetupProcessState.Error)
-				.OnEntryFrom(_errorTrigger, ex => _payloader.HandleError(ex))
-				.OnEntry(a=>_payloader.HandleError(null));
-
 			_errorTrigger = _machine.SetTriggerParameters<Exception>(SetupEvent.Error);
+
+			_machine.Configure(SetupProcessState.Error)
+				.OnEntryFrom(_errorTrigger, OnError)
+				.OnEntry(a => OnError(null));
+
 		}
 
-		private StateMachine<SetupMachine.SetupProcessState, SetupMachine.SetupEvent>.TriggerWithParameters<Exception> _errorTrigger;
-
-		public void FireError(Exception ex) // TODO: REFACTOR THIS
+		private void FireError(Exception ex)
 		{
 			_machine.Fire(_errorTrigger, ex);
+		}
+
+		private void TryFire(SetupEvent e)
+		{
+			if (_machine == null)
+				throw new InvalidOperationException("Invoke SetMachine first");
+			if (!_machine.CanFire(e))
+				throw new InvalidOperationException(string.Format("Cannot transit from {0}", _machine.State));
+			if (e == SetupEvent.Error)
+				throw new InvalidOperationException("Use FireError");
+			_machine.Fire(e);
+		}
+
+		private void OnNotStarted(
+			StateMachine<SetupProcessState, SetupEvent>.Transition transtitionData)
+		{
+			TryFire(SetupEvent.Inject);
+		}
+
+		private void OnInjectServices(StateMachine<SetupProcessState, SetupEvent>.Transition transition)
+		{
+			try
+			{
+				_payloader.InjectServices();
+			}
+			catch (Exception ex)
+			{
+				FireError(ex);
+			}
+			TryFire(SetupEvent.Injected);
+		}
+
+		private void OnServicesInjected(StateMachine<SetupProcessState, SetupEvent>.Transition transition)
+		{
+			TryFire(SetupEvent.Upgrade);
+		}
+
+		private void OnUpgrading(StateMachine<SetupProcessState, SetupEvent>.Transition transition)
+		{
+			try
+			{
+				_payloader.RunUpgrades();
+			}
+			catch (Exception ex)
+			{
+				FireError(ex);
+			}
+			TryFire(SetupEvent.Upgraded);
+		}
+
+		public void OnError(Exception ex)
+		{
+			if (ex == null) throw new ArgumentNullException("ex");
+
+			//TODO: LOG??
+			throw new ermeXSetupException(ex);
 		}
 	}
 }
--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/SetupMachineTester.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/SetupMachineTester.cs
@@ -5,38 +5,85 @@ using System.Text;
 using Moq;
 using NUnit.Framework;
 using ermeX.ComponentServices.ComponentSetup;
+using ermeX.Exceptions;
 
 namespace ermeX.Tests.ComponentServices
 {
-	//this is tests the setupmachine integrated with the payloader
 	[TestFixture]
 	public class SetupMachineTester
 	{
-
 		[Test]
 		public void CanSetup()
 		{
 			var context=new TestContext();
+			context.WithNormalWorkflow();
 			var target = new SetupMachine(context.Payloader);
 			target.Setup();
 
 			Assert.AreEqual(SetupMachine.SetupProcessState.Ready, target.State);
+
+			context.Verify();
 		}
 
 		[Test]
-		public void OnErrorThrows()
+		public void OnErrorInjecting_GoesToError()
 		{
-			throw new NotImplementedException("Do for all states");
+			var context = new TestContext();
+			context.WithInjectingFailure();
+			VerifyTransitionToError(context);
+		}
+
+		[Test]
+		public void OnErrorUpgrading_GoesToError()
+		{
+			var context = new TestContext();
+			context.WithUpgradingFailure();
+			VerifyTransitionToError(context);
+		}
+
+		private static void VerifyTransitionToError(TestContext context)
+		{
+			var target = new SetupMachine(context.Payloader);
+			Assert.Throws<ermeXSetupException>(target.Setup);
+
+			Assert.AreEqual(SetupMachine.SetupProcessState.Error, target.State);
+
+			context.Verify();
 		}
 
 		private class TestContext
 		{
 			public TestContext()
 			{
-				Payloader=new SetupPayloader();
+				_payloader=new Mock<ISetupPayloader>();
 			}
 
-			public ISetupPayloader Payloader { get; private set; }
+			public void WithNormalWorkflow()
+			{
+				_payloader.Setup(x=>x.InjectServices()).Verifiable();
+				_payloader.Setup(x => x.RunUpgrades()).Verifiable();
+			}
+
+			public void WithInjectingFailure()
+			{
+				_payloader.Setup(x=>x.InjectServices()).Throws(new Exception("Test")).Verifiable();
+			}
+
+			public void WithUpgradingFailure()
+			{
+				_payloader.Setup(x => x.RunUpgrades()).Throws(new Exception("Test")).Verifiable();
+			}
+
+			private readonly Mock<ISetupPayloader> _payloader;
+			public ISetupPayloader Payloader
+			{
+				get { return _payloader.Object; }
+			}
+
+			public void Verify()
+			{
+				_payloader.Verify();
+			}
 		}
 	}
 }
