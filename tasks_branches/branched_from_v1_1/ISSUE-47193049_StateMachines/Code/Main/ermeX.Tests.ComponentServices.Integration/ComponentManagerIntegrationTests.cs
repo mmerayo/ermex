95f1e7b3522713f52f7420070ca7b0bedf8e7abc
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-13 17:14:12 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Added some integration tests to the component manager setup

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices.Integration/ComponentManagerIntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ermeX.ComponentServices;
+using ermeX.Configuration;
+using ermeX.Tests.Common.Networking;
+
+namespace ermeX.Tests.ComponentServices.Integration
+{
+	[TestFixture]
+	public class ComponentManagerIntegrationTests
+	{
+		private static Configurer GetTestConfiguration()
+		{
+			var testPort = new TestPort(15235);
+
+			return Configurer.Configure(Guid.NewGuid()).ListeningToTcpPort(testPort)
+							 .RequestJoinTo("127.0.0.1", new TestPort(25552), Guid.NewGuid())
+							 .SetInMemoryDb();
+		}
+
+		[Test]
+		public void CanSetup()
+		{
+			Configurer settings = GetTestConfiguration();
+			var target = ComponentManager.Default;
+			target.Setup(settings);
+			Assert.IsTrue(target.IsRunning());
+		}
+
+		[Test]
+		public void CanSetUpAndReset()
+		{
+			Configurer settings = GetTestConfiguration();
+			var target = ComponentManager.Default;
+			target.Setup(settings);
+			Assert.IsTrue(target.IsRunning());
+			target.Reset();
+			Assert.IsFalse(target.IsRunning());
+		}
+
+		[Test]
+		public void CanResetAndSetUp()
+		{
+			Configurer settings = GetTestConfiguration();
+			var target = ComponentManager.Default;
+			target.Reset();
+			Assert.IsFalse(target.IsRunning());
+			target.Setup(settings);
+			Assert.IsTrue(target.IsRunning());
+		}
+
+		[Test]
+		public void CanSetupSeveralTimes()
+		{
+			Configurer settings = GetTestConfiguration();
+			var target = ComponentManager.Default;
+			for (int i = 0; i < 5; i++)
+			{
+				target.Setup(settings);
+				Assert.IsTrue(target.IsRunning());
+			}
+		}
+
+		[Test]
+		public void CanSetupAndResetSeveralTimes()
+		{
+			Configurer settings = GetTestConfiguration();
+			var target = ComponentManager.Default;
+			for (int i = 0; i < 5; i++)
+			{
+				target.Setup(settings);
+				Assert.IsTrue(target.IsRunning());
+				target.Reset();
+				Assert.IsFalse(target.IsRunning());
+			}
+		}
+	}
+}
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ermeX.Tests.ComponentServices.Integration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e502d447-7c51-4dd8-8217-8084f6f4edb3")]
