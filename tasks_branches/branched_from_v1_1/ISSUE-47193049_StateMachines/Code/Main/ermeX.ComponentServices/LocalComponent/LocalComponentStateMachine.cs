ff9ca35a1ac8459b9e79cd18d996bc0b43192652
mmerayo30 <mmerayo30@gmail.com>
2013-05-14 15:46:23 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. improved statemachine local component

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponentStateMachine.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponentStateMachine.cs
@@ -1,6 +1,33 @@
-﻿namespace ermeX.ComponentServices.LocalComponent
+﻿using Common.Logging;
+using ermeX.ComponentServices.ComponentSetup;
+
+namespace ermeX.ComponentServices.LocalComponent
 {
-	internal class LocalComponentStateMachine
+	internal sealed class LocalComponentStateMachine
 	{
+		private static readonly ILog Logger = LogManager.GetLogger<LocalComponentStateMachine>();
+
+		private enum MachineEvent
+		{
+			Start,
+			SubscribeToMessages,
+			PublishServices,
+			Run,
+			Stop,
+			Reset,
+			ToError
+		}
+
+		private enum MachineState
+		{
+			Stopped = 0,
+			Starting,
+			SubscribingMessageHandlers,
+			PublishingServices,
+			Running,
+			Stopping,
+			Resetting,
+			Error
+		}
 	}
 }
