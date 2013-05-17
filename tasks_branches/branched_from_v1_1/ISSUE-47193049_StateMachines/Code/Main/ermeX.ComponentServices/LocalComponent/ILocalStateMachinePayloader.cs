8627dc101d969f3e7a1081b4e661d29b98ff5142
mmerayo30 <mmerayo30@gmail.com>
2013-05-17 10:37:56 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. moved to own class state machine local component

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/ILocalStateMachinePayloader.cs
@@ -0,0 +1,12 @@
+﻿namespace ermeX.ComponentServices.LocalComponent
+{
+	internal interface ILocalStateMachinePayloader
+	{
+		void Reset();
+		void Stop();
+		void Run();
+		void PublishServices();
+		void SubscribeToMessages();
+		void Start();
+	}
+}
--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponent.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponent.cs
@@ -7,15 +7,57 @@ using Stateless;
 
 namespace ermeX.ComponentServices.LocalComponent
 {
-	internal sealed partial class LocalComponent:ILocalComponent
+	internal sealed class LocalComponent:ILocalComponent,ILocalStateMachinePayloader
 	{
 		private static readonly ILog Logger = LogManager.GetLogger<LocalComponent>();
+		private readonly LocalComponentStateMachine _stateMachine;
 
 		public LocalComponent()
 		{
-			DefineStateMachineTransitions();
+			_stateMachine = new LocalComponentStateMachine(this);
 		}
 
-		
+
+		public void Reset()
+		{
+			Logger.Debug("Reset");
+			throw new NotImplementedException();
+		}
+
+		public void Stop()
+		{
+			Logger.Debug("Stop");
+			throw new NotImplementedException();
+		}
+
+		public void Run()
+		{
+			Logger.Debug("Run");
+			throw new NotImplementedException();
+		}
+
+		public void PublishServices()
+		{
+			Logger.Debug("PublishServices");
+			throw new NotImplementedException();
+		}
+
+		public void SubscribeToMessages()
+		{
+			Logger.Debug("SubscribeToMessages");
+			throw new NotImplementedException();
+		}
+
+		public void Start()
+		{
+			Logger.Debug("Start");
+			_stateMachine.Start();
+		}
+
+		public bool IsErrored()
+		{
+			Logger.Debug("IsErrored");
+			return _stateMachine.IsErrored();
+		}
 	}
 }
