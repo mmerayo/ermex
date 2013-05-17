49aa42ba76a379c3d73efe3baeaedc4e2f498c94
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-17 12:35:21 +0000

    ISSUE-47193049: Branch commit. Added localmachine test scaffold

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/LocalComponent/LocalComponentStateMachineTester.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ermeX.Tests.ComponentServices.LocalComponent
+{
+	[TestFixture]
+	class LocalComponentStateMachineTester
+	{
+		[Test]
+		public void CanRun()
+		{
+			//check all substates
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanStop()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromStarting()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void CanTransitToError_FromSubscribing()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void CanTransitToError_FromPublishing()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void CanTransitToError_FromRunning()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void CanTransitToError_FromStopping()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromResetting()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitTo_ResetErrorResetStopped()
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
