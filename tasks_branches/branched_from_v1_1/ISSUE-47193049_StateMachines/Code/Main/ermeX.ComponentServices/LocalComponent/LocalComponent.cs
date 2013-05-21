2e631fe09567056087267b788d79a4a34de63152
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-21 16:37:32 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. minor change

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponent.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponent.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Common.Logging;
-using Stateless;
-using ermeX.Bus.Interfaces;
-using ermeX.Bus.Listening.Handlers.InternalMessagesHandling.WorkflowHandlers;
-using ermeX.Bus.Publishing.Dispatching.Messages;
-using ermeX.ComponentServices.LocalComponent.Commands;
+﻿using Common.Logging;
 using ermeX.ConfigurationManagement.Settings;
-using ermeX.DAL.Interfaces.Component;
 
 namespace ermeX.ComponentServices.LocalComponent
 {
-	internal sealed class LocalComponent:ILocalComponent
+	internal sealed class LocalComponent : ILocalComponent
 	{
 		private static readonly ILog Logger = LogManager.GetLogger<LocalComponent>();
 		private readonly LocalComponentStateMachine _stateMachine;
 
-		public LocalComponent(IBizSettings settings)
+		public LocalComponent(LocalComponentStateMachine stateMachine)
 		{
-			//TODO: INJECT
-			_stateMachine = new LocalComponentStateMachine(new LocalComponentStateMachineHandler(this,settings));
+			_stateMachine = stateMachine;
 		}
 
 		public void Start()
@@ -29,85 +18,5 @@ namespace ermeX.ComponentServices.LocalComponent
 			Logger.Debug("Start");
 			_stateMachine.Start();
 		}
-
-
-		private sealed class LocalComponentStateMachineHandler:ILocalStateMachinePayloader
-		{
-			private readonly IStartablesStarterStepExecutor _startablesStarter;
-			private readonly ISubscribeToMessagesStepExecutor _messagesSubscriber;
-			private readonly IPublishServicesStepExecutor _servicesPublisher;
-			private readonly IResetStepExecutor _resetExecutor;
-			private readonly IStopComponentStepExecutor _stopExecutor;
-			private readonly IRunStepExecutor _runExecutor;
-			private readonly IErrorStepExecutor _errorExecutor;
-
-			public LocalComponentStateMachineHandler(
-				IStartablesStarterStepExecutor startablesStarter,
-				ISubscribeToMessagesStepExecutor messagesSubscriber,
-				IPublishServicesStepExecutor servicesPublisher, 
-				IResetStepExecutor resetExecutor,
-				IStopComponentStepExecutor stopExecutor,
-				IRunStepExecutor runExecutor,
-				IErrorStepExecutor errorExecutor)
-			{
-				_startablesStarter = startablesStarter;
-				_messagesSubscriber = messagesSubscriber;
-				_servicesPublisher = servicesPublisher;
-				_resetExecutor = resetExecutor;
-				_stopExecutor = stopExecutor;
-				_runExecutor = runExecutor;
-				_errorExecutor = errorExecutor;
-			}
-
-			public void Reset()
-			{
-				Logger.Debug("Reset");
-				
-				_resetExecutor.Reset();
-			}
-
-			public void Stop()
-			{
-				Logger.Debug("Stop");
-				_stopExecutor.Stop();
-			}
-
-			public void Run()
-			{
-				Logger.Debug("Run");
-
-				_runExecutor.Run();
-			}
-
-			public void PublishServices()
-			{
-				Logger.Debug("PublishServices");
-
-				_servicesPublisher.Publish();
-			}
-
-			public void SubscribeToMessages()
-			{
-				Logger.Debug("SubscribeToMessages");
-
-				_messagesSubscriber.Subscribe();
-			}
-
-			public void OnStart()
-			{
-				Logger.Debug("OnStart");
-				
-				_startablesStarter.DoStart();
-				
-			}
-
-			public void Error()
-			{
-				Logger.Debug("Error");
-				
-				_errorExecutor.OnError();
-			}
-		}
-
 	}
 }
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/LocalComponent/LocalComponentStateMachineHandler.cs
@@ -0,0 +1,86 @@
+﻿using Common.Logging;
+using ermeX.ComponentServices.LocalComponent.Commands;
+
+namespace ermeX.ComponentServices.LocalComponent
+{
+	internal sealed class LocalComponentStateMachineHandler : ILocalStateMachinePayloader
+	{
+		private static readonly ILog Logger = LogManager.GetLogger<LocalComponentStateMachineHandler>();
+
+		private readonly IStartablesStarterStepExecutor _startablesStarter;
+		private readonly ISubscribeToMessagesStepExecutor _messagesSubscriber;
+		private readonly IPublishServicesStepExecutor _servicesPublisher;
+		private readonly IResetStepExecutor _resetExecutor;
+		private readonly IStopComponentStepExecutor _stopExecutor;
+		private readonly IRunStepExecutor _runExecutor;
+		private readonly IErrorStepExecutor _errorExecutor;
+
+		public LocalComponentStateMachineHandler(
+			IStartablesStarterStepExecutor startablesStarter,
+			ISubscribeToMessagesStepExecutor messagesSubscriber,
+			IPublishServicesStepExecutor servicesPublisher,
+			IResetStepExecutor resetExecutor,
+			IStopComponentStepExecutor stopExecutor,
+			IRunStepExecutor runExecutor,
+			IErrorStepExecutor errorExecutor)
+		{
+			_startablesStarter = startablesStarter;
+			_messagesSubscriber = messagesSubscriber;
+			_servicesPublisher = servicesPublisher;
+			_resetExecutor = resetExecutor;
+			_stopExecutor = stopExecutor;
+			_runExecutor = runExecutor;
+			_errorExecutor = errorExecutor;
+		}
+
+		public void Reset()
+		{
+			Logger.Debug("Reset");
+
+			_resetExecutor.Reset();
+		}
+
+		public void Stop()
+		{
+			Logger.Debug("Stop");
+
+			_stopExecutor.Stop();
+		}
+
+		public void Run()
+		{
+			Logger.Debug("Run");
+
+			_runExecutor.Run();
+		}
+
+		public void PublishServices()
+		{
+			Logger.Debug("PublishServices");
+
+			_servicesPublisher.Publish();
+		}
+
+		public void SubscribeToMessages()
+		{
+			Logger.Debug("SubscribeToMessages");
+
+			_messagesSubscriber.Subscribe();
+		}
+
+		public void OnStart()
+		{
+			Logger.Debug("OnStart");
+
+			_startablesStarter.DoStart();
+
+		}
+
+		public void Error()
+		{
+			Logger.Debug("Error");
+
+			_errorExecutor.OnError();
+		}
+	}
+}
