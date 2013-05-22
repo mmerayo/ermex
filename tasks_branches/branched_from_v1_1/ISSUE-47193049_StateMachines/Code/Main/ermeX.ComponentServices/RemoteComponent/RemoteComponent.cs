3b0317ad91f6ac3e5126373029537d22c5bad3e8
mmerayo30 <mmerayo30@gmail.com>
2013-05-22 09:24:20 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Started creating remote component state machine

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/RemoteComponent/RemoteComponent.cs
@@ -0,0 +1,18 @@
+﻿using Ninject;
+using ermeX.DAL.Interfaces.Component;
+
+namespace ermeX.ComponentServices.RemoteComponent
+{
+	internal class RemoteComponent : IRemoteComponent
+	{
+
+		public RemoteComponent()
+		{
+		}
+
+		public void Join()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
