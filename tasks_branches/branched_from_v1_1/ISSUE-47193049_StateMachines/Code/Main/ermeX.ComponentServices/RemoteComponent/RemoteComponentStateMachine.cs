bd0eae742dc84e0412f632a2173c51221a1f7aee
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-22 10:17:55 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Restore point Creating remote component state machine

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/RemoteComponent/RemoteComponentStateMachine.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/RemoteComponent/RemoteComponentStateMachine.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Common.Logging;
+using Ninject;
+using Stateless;
+using ermeX.ComponentServices.LocalComponent;
+using ermeX.Exceptions;
 
 namespace ermeX.ComponentServices.RemoteComponent
 {
@@ -24,6 +29,114 @@ namespace ermeX.ComponentServices.RemoteComponent
 			Joining,
 			Joined,
 			Errored
+		}
+		private static readonly ILog Logger = LogManager.GetLogger<RemoteComponentStateMachine>();
+
+		private readonly StateMachine<RemoteComponentState, RemoteComponentEvent> _machine =
+			new StateMachine<RemoteComponentState, RemoteComponentEvent>(RemoteComponentState.Stopped);
+
+		private StateMachine<RemoteComponentState, RemoteComponentEvent>.TriggerWithParameters<Exception> _errorTrigger;
+
+		[Inject]
+		public RemoteComponentStateMachine()
+		{
+			DefineStateMachineTransitions();
+		}
+
+		private void DefineStateMachineTransitions()
+		{
+			Logger.Debug("DefineStateMachineTransitions");
+			_machine.Configure(RemoteComponentState.Prelive)
+					.OnEntry(OnPrelive)
+					.Permit(RemoteComponentEvent.Create, RemoteComponentState.Created);
+
+			_machine.Configure(RemoteComponentState.Created)
+					.OnEntry(OnCreated)
+					.Permit(RemoteComponentEvent.Ready, RemoteComponentState.Stopped);
+
+			_machine.Configure(RemoteComponentState.Stopped)
+			        .OnEntry(OnStopped)
+			        .Permit(RemoteComponentEvent.Join, RemoteComponentState.Joining);
+
+			_machine.Configure(RemoteComponentState.Joining)
+			        .OnEntry(OnJoining)
+			        .Permit(RemoteComponentEvent.Joined, RemoteComponentState.Joined)
+			        .Permit(RemoteComponentEvent.ToError, RemoteComponentState.Errored);
+
+			_machine.Configure(RemoteComponentState.Joined)
+			        .OnEntry(OnJoined)
+			        .Permit(RemoteComponentEvent.ToError, RemoteComponentState.Errored)
+			        .Permit(RemoteComponentEvent.Stop, RemoteComponentState.Stopped);
+
+			_machine.Configure(RemoteComponentState.Errored)
+					.OnEntry(OnErrored)
+					.Permit(RemoteComponentEvent.Stop, RemoteComponentState.Stopped);
+
+
+			_errorTrigger = _machine.SetTriggerParameters<Exception>(RemoteComponentEvent.ToError);
+
+			_machine.Configure(RemoteComponentState.Errored)
+					.OnEntryFrom(_errorTrigger, OnError)
+					.OnEntry(a => OnError(null));
+		}
+
+		private void FireError(Exception ex)
+		{
+			Logger.WarnFormat("FireError-{0}", ex.ToString());
+			if (IsErrored())
+				throw ex;
+			_machine.Fire(_errorTrigger, ex);
+		}
+
+
+		private void TryFire(RemoteComponentEvent e)
+		{
+			Logger.DebugFormat("TryFire - {0}", e);
+			if (_machine == null)
+				throw new ApplicationException("FATAL");
+			if (!_machine.CanFire(e))
+				throw new InvalidOperationException(string.Format("Cannot transit from:{0} with trigger:{1}", _machine.State, e));
+			if (e == LocalComponentEvent.ToError)
+				throw new InvalidOperationException("Use FireError");
+			_machine.Fire(e);
+		}
+
+		private void OnError(Exception ex)
+		{
+			if (ex == null) throw new ArgumentNullException("ex");
+			Logger.DebugFormat("OnError - {0}", ex.ToString());
+			_errorExecutor.OnError(); //TODO: MOVE NEXT LINE TO THE EXECUTOR
+			throw new ermeXRemoteComponentException(ex);
+		}
+
+		private void OnErrored(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnJoined(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnJoining(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnStopped(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnCreated(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnPrelive(StateMachine<RemoteComponentState, RemoteComponentEvent>.Transition obj)
+		{
+			throw new NotImplementedException();
 		}
 
 
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Exceptions/ermeXLocalComponentException.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace ermeX.Exceptions
+{
+	public class ermeXLocalComponentException : ermeXException
+	{
+		public ermeXLocalComponentException(string message)
+			: base(message)
+		{
+		}
+
+		public ermeXLocalComponentException(Exception innerException)
+			: this("An exception ocurred while starting the ermeX local component ", innerException) { }
+		public ermeXLocalComponentException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		public ermeXLocalComponentException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
+}
