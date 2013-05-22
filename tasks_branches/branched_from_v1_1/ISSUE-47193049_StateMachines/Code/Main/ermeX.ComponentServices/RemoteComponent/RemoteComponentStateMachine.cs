1cf2b183b40abd2200a18ea8578fee36dd4e487d
mmerayo30 <mmerayo30@gmail.com>
2013-05-22 09:38:07 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Restore point Creating remote component state machine

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/RemoteComponent/RemoteComponentStateMachine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ermeX.ComponentServices.RemoteComponent
+{
+	internal sealed class RemoteComponentStateMachine:IRemoteComponentStateMachine
+	{
+		private enum RemoteComponentEvent
+		{
+			Create=0,
+			Ready,
+			Join,
+			Joined,
+			ToError,
+			Stop
+		}
+		private enum RemoteComponentState
+		{
+			Prelive=0,
+			Created,
+			Stopped,
+			Joining,
+			Joined,
+			Errored
+		}
+
+
+		public void Create()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Join()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Stop()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsErrored()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsStopped()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsRunning()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
