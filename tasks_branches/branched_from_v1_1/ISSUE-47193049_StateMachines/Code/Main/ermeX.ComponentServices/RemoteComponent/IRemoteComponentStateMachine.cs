de8a4804927fea1efc9800473578ad082b5f0738
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-22 09:38:07 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Restore point Creating remote component state machine

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.ComponentServices/RemoteComponent/IRemoteComponentStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ermeX.ComponentServices.RemoteComponent
+{
+	internal interface IRemoteComponentStateMachine
+	{
+		void Create();
+		void Join();
+		void Stop();
+
+		bool IsErrored();
+		bool IsStopped();
+		bool IsRunning();
+	}
+}
