ce39c959202872ff7632453fc9b86f804df8bd83
mmerayo30 <mmerayo30@gmail.com>
2013-05-23 12:16:59 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Restore point Creating remote component state machine. Tests

--- tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/RemoteComponent/RemoteComponentStateMachineTester.cs
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/RemoteComponent/RemoteComponentStateMachineTester.cs
@@ -10,7 +10,103 @@ namespace ermeX.Tests.ComponentServices.RemoteComponent
 	class RemoteComponentStateMachineTester
 	{
 		[Test]
-		public void TODO(){throw new NotImplementedException();}
+		public void InitsInPrelive()
+		{
+			throw new NotImplementedException();
+		}
 
+		[Test]
+		public void CanCreate()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanGetReady()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void CanJoin()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanRun()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void WhenRunningRequestsServices()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void WhenRunningRequestsSubscriptions()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void WhenRunningCanReceiveServices()
+		{
+			throw new NotImplementedException();
+		}
+		[Test]
+		public void WhenRunningCanReceiveSubscriptions()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromPrelive()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromCreating()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromJoining()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromRunning()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromRequestSubscriptions()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromSubscriptionsReceived()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromRequestServices()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Test]
+		public void CanTransitToError_FromServicesReceived()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
