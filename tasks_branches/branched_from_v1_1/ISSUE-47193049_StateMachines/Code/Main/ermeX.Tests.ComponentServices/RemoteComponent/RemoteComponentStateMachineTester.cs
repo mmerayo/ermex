d1d2aa260e47a49a0a4c5130a36db638cc075dcd
mmerayo30 <mmerayo30@gmail.com>
2013-05-23 11:47:23 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Restore point Creating remote component state machine. Tests

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.ComponentServices/RemoteComponent/RemoteComponentStateMachineTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ermeX.Tests.ComponentServices.RemoteComponent
+{
+	[TestFixture]
+	class RemoteComponentStateMachineTester
+	{
+		[Test]
+		public void TODO(){throw new NotImplementedException();}
+
+	}
+}
