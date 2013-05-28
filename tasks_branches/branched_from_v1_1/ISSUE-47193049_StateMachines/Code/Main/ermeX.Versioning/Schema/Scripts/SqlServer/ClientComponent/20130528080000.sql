cedc933bfe91a9ca5fd80acdfcf20da449f0ed93
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-05-28 09:26:35 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. sCRIPTS TO REMOVE COLUMNS

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Versioning/Schema/Scripts/SqlServer/ClientComponent/20130528080000.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [ClientComponent].[Components] DROP COLUMN [Components_IsRunning]
+GO
+ALTER TABLE [ClientComponent].[Components] DROP COLUMN [Components_ExchangedDefinitions]
+GO
+ALTER TABLE [ClientComponent].[Components] DROP COLUMN [Components_ComponentExchanges]
+GO
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Versioning/Schema/Scripts/Sqlite/ClientComponent/20130528080000.sql
@@ -0,0 +1,26 @@
+﻿
+BEGIN TRANSACTION;
+CREATE TEMPORARY TABLE Components_backup(
+    Components_Id                   INTEGER PRIMARY KEY AUTOINCREMENT
+                                            NOT NULL
+                                            UNIQUE,
+    Components_ComponentId          GUID    NOT NULL,
+    Components_ComponentOwner       GUID    NOT NULL,
+    Components_Version              INTEGER NOT NULL,
+    Components_Latency              INTEGER NOT NULL,);
+
+INSERT INTO Components_backup SELECT Components_Id,Components_ComponentId,Components_ComponentOwner,Components_Version,Components_Latency FROM Components;
+DROP TABLE Components;
+CREATE TABLE Components (
+	Components_Id                   INTEGER PRIMARY KEY AUTOINCREMENT 
+											NOT NULL
+                                            UNIQUE,
+    Components_ComponentId          GUID    NOT NULL,
+    Components_ComponentOwner       GUID    NOT NULL,
+    Components_Version              INTEGER NOT NULL,
+    Components_Latency              INTEGER NOT NULL,);
+INSERT INTO Components SELECT Components_Id,Components_ComponentId,Components_ComponentOwner,Components_Version,Components_Latency FROM Components_backup;
+DROP TABLE Components_backup;
+COMMIT;
+GO
+
