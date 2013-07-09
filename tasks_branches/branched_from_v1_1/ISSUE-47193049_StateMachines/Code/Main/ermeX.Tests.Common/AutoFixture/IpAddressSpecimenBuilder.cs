4c1d3937417962327f3671e6fc2150dcfe057c9c
mmerayo30@gmail.com <mmerayo30@gmail.com@a640d4ee-1192-c04a-60bd-759ea150f6e5>
2013-07-09 14:51:05 +0000

    [https://www.pivotaltracker.com/story/show/47193049]-Branch commit. Ensure other tests keep running

---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.Common/AutoFixture/IpAddressSpecimenBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Ploeh.AutoFixture.Kernel;
+
+namespace ermeX.Tests.Common.AutoFixture
+{
+	public class IpAddressSpecimenBuilder : ISpecimenBuilder
+	{
+		private static readonly Random _random = new Random();
+
+		public object Create(object request, ISpecimenContext context)
+		{
+			if (Equals(request, typeof(IPAddress)))
+			{
+				var bytes = new[] { (byte)_random.Next(0, 255), (byte)_random.Next(0, 255), (byte)_random.Next(0, 255), (byte)_random.Next(0, 255) };
+
+				return new IPAddress(bytes);
+			}
+
+			return new NoSpecimen(request);
+		}
+	}
+}
---
+++ tasks_branches/branched_from_v1_1/ISSUE-47193049_StateMachines/Code/Main/ermeX.Tests.Common/AutoFixture/ermexCustomizations.cs
@@ -0,0 +1,14 @@
+﻿using Ploeh.AutoFixture;
+
+namespace ermeX.Tests.Common.AutoFixture
+{
+	public class ErmeXCustomizations : ICustomization
+	{
+		public void Customize(IFixture fixture)
+		{
+			fixture.Customize(new MultipleCustomization());
+
+			fixture.Customizations.Add(new IpAddressSpecimenBuilder());
+		}
+	}
+}
